070a64c33579be8ec9b399a2980395f868f0ea57
MasterBehzadi <MasterBehzadi@gmail.com>
2022-11-17 18:55:30 +0330

    Update
    Adding StyleSheets and Stating Code Of Scripts
    
    Co-Authored-By: MohsenEMX <78661162+mohsenemx@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/style.css">
+    <title>Upload File</title>
+</head>
+<body>
+        <form action="https://cloud.midline.ir/gjtV2SC7oKiPoNuxfCnkTS9Pw66qwccOTFnXRkQowxbT8gPbUry_B9-9HEXYAg4rTLBlun5fE9HbOMghRmMpq6q2LKdXeE2PXEjrM912V8DwkY6EOQBq5hu7e0YQH0_M4muAvVwMESuxSKI8/uploadFile" method="post" enctype="multipart/form-data">
+        <label for="file" class="main">یک فایل انتخاب کنید:</label>
+        <input type="file" id="file" name="file"><br />
+        <input type="submit" value="آپلود فایل" class="main submit">
+    </form>
+    </body>
+</html>
