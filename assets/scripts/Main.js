f715681f7ef59d91e2865808836d349324ef6da5
MasterBehzadi <MasterBehzadi@gmail.com>
2022-11-18 23:38:14 +0330

    the little updarte
    Co-Authored-By: MohsenEMX <78661162+mohsenemx@users.noreply.github.com>

--- assets/scripts/Main.js
+++
@@ -1,9 +0,0 @@
-    if (she == crush) {
-        love_her();
-    }
-    
-    if (she == girlfriend) {
-        love_her_more();
-        make_her_happy();
-        sex_with_her();
-    }
---
+++ assets/scripts/MainScript.js
@@ -0,0 +1,11 @@
+// Rakab Khordi Keykavoos, in Script Aslan Script Asli Nabood :)!!!
+if (she == crush) {
+
+        if (she == girlfriend) {
+            love_her_more();
+            make_her_happy();
+            sex_with_her();
+        } else {
+            love_her();  
+        }
+    }
