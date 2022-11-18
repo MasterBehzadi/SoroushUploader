cc8161f73fbeca5006d911f82046494572597fd3
MasterBehzadi <MasterBehzadi@gmail.com>
2022-11-18 19:33:42 +0330

    Update Por Mohtava
    Co-Authored-By: MohsenEMX <78661162+mohsenemx@users.noreply.github.com>

--- index.php
+++ index.php
@@ -8,14 +8,13 @@
     <link rel="stylesheet" href="/assets/stylesheets/style.css">
     <title>Upload File</title>
     <script src="/assets/scripts/side.js"></script>
+    <script src="/assets/scripts/Main.js"></script>
 </head>
 <body onload="dumb()">
         <form method="POST" enctype="multipart/form-data" action="/upload.php">
         <label for="file" class="main">یک فایل انتخاب کنید:</label>
         <input type="file" id="file" name="file"><br />
         <input type="submit" value="آپلود فایل" class="main submit">
-    </form>
-    </body>
-</html>
-
+        </form>    
+<
 <!-- Cloud Action : https://cloud.midline.ir/gjtV2SC7oKiPoNuxfCnkTS9Pw66qwccOTFnXRkQowxbT8gPbUry_B9-9HEXYAg4rTLBlun5fE9HbOMghRmMpq6q2LKdXeE2PXEjrM912V8DwkY6EOQBq5hu7e0YQH0_M4muAvVwMESuxSKI8/uploadFile -->
--- upload2.php
+++ upload2.php
@@ -7,31 +7,23 @@ $web_page_to_send = "https://cloud.midline.ir/gjtV2SC7oKiPoNuxfCnkTS9Pw66qwccOTF
 $theFile = $_FILES['file'];
 // $_SERVER['DOCUMENT_ROOT']."/images/test.jpg"
 
-echo $file_name_with_full_path;
-echo "\r\n";
-
+// mantegh -=- -> Pedaret <- 
 $post_request = array(
     // "sender" => "TheAmplituhedron", 
-	"file" => curl_file_create($theFile) // for php 5.5+
+	"file" => curl_file_create($theFile) //### /tempFiles/Movaghat.zip (Bayad Patch Be File Fiziki Bedim) !!!!
 );
 $ch = curl_init();
-echo $ch;
-echo "\r\n";
 curl_setopt($ch, CURLOPT_URL, $web_page_to_send);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_request);
 $result = curl_exec($ch); // >>> 0 >>> Failed
 if ($result == 0) {
-    echo 'Failed';
+    echo 'Failed DaEI ';
 } else {
     echo 'Not Failed';
 }
-echo $result;
 curl_close($ch);
-echo $result;
-echo "\r\n";
 $xml=simplexml_load_string($result) or die("Error: Cannot create object");
-echo "\r\n";
 print_r($xml);
 ?>
 
@@ -53,3 +45,10 @@ print_r($xml);
     <p><?php echo $result ?></p>
     <a href="https://cloud.midline.ir/<?php echo $BotToken ?>/downloadFile/<?php echo $Token ?>"><p>https://cloud.midline.ir/<?php echo $BotToken ?>/downloadFile/<?php echo $Token ?></p></a>
 </body>
+
+
+
+
+[18-Nov-2022 19:02:27 Asia/Tehran] PHP Warning:  curl_file_create() expects parameter 1 to be a valid path, array given in /home/igangir/public_html/cgi-bin/cd/upload.php on line 14
+[18-Nov-2022 19:02:27 Asia/Tehran] PHP Warning:  curl_setopt(): Filename cannot be empty in /home/igangir/public_html/cgi-bin/cd/upload.php on line 19
+[18-Nov-2022 19:02:27 Asia/Tehran] PHP Warning:  curl_exec(): Filename cannot be empty in /home/igangir/public_html/cgi-bin/cd/upload.php on line 20
